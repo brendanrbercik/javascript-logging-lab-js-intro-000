72e8403e8586c078c14debd690d7fba17e6c02b7
Brendan Bercik <brendanrbercik@gmail.com>
2020-03-19 21:37:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 console.error("oh no!")
 console.log("checking in " + 1000)
 console.warn("sure you got this one bub?")
+
+//declare the variable
+
+var word
+
+// assign a value to the variable
+world = "bird"
+
+console.log(word) // ' bird'
+
+// assign another value to the variable
+word = 'cat'
+
+console.log(word)
